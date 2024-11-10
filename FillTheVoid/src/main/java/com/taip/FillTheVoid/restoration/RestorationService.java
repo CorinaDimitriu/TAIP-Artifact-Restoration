ebd7acafe2b2c518224b16d001fd33d244100f0c
CarceaRazvan <bivocar@gmail.com>
2024-11-10 21:49:23 +0200

    restoration python.exe file

--- FillTheVoid/src/main/java/com/taip/FillTheVoid/restoration/RestorationService.java
+++ FillTheVoid/src/main/java/com/taip/FillTheVoid/restoration/RestorationService.java
@@ -2,6 +2,17 @@ package com.taip.FillTheVoid.restoration;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+
+
 @Service
 @AllArgsConstructor
 public class RestorationService {
@@ -18,11 +29,122 @@ public class RestorationService {
         System.out.println("Here use pythonic code to restore and obtain new restoredImage");
         System.out.println(corners);
 
+
+        saveAsPng(image);
+
+//        run scipt
+
+        List<String> allLinesRestoration = runScriptRestoration();
+        System.out.println(allLinesRestoration);
+
+
+
         // TODO restoredImage should be changed (in this example is returned same image)
-        byte[] restoredImage = image;
+        byte[] restoredImage = readImage("src/main/java/com/taip/FillTheVoid/restoration/restored_image.png");
+
 
         RestoredImage newRestoredImage = new RestoredImage(imageType, restoredImage);
 
+
         return newRestoredImage;
+    }
+
+    private void writeBase64ToFile(String base64Image) {
+        // Specify the path of the file where the Base64 string should be written
+        String filePath = "src/main/java/com/taip/FillTheVoid/restoration/output_image.txt";
+
+        // Create a new File object
+        File file = new File(filePath);
+
+        try {
+            // Check if the file exists, if not create it
+            if (!file.exists()) {
+                if (file.createNewFile()) {
+                    System.out.println("File created: " + filePath);
+                } else {
+                    System.err.println("Failed to create the file.");
+                    return;
+                }
+            }
+
+            // Write the Base64 string to the file
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                writer.write(base64Image);
+                System.out.println("Base64 string written to " + filePath);
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+
+    }
+
+    public void saveAsPng(byte[] image) {
+        BufferedImage bufferedImage = null;
+        try {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(image);
+            bufferedImage = ImageIO.read(byteArrayInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Specify the output file path where you want to save the PNG
+        String outputFilePath = "src/main/java/com/taip/FillTheVoid/restoration/restored_image.png";
+
+        // Save the BufferedImage to a file as PNG
+        try {
+            if (bufferedImage != null) {
+                File outputFile = new File(outputFilePath);
+                ImageIO.write(bufferedImage, "PNG", outputFile);
+                System.out.println("Image saved as PNG at: " + outputFilePath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static byte[] readImage(String filePath) {
+        Path path = Paths.get(filePath);
+        byte[] imageBytes = null;
+        try {
+            imageBytes = Files.readAllBytes(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return imageBytes;
+    }
+
+    public List<String> runScriptRestoration() {
+
+        String pythonExecutable = "myenv/bin/python";
+        String pythonScriptPath = "src/main/java/com/taip/FillTheVoid/restoration/restoration.py";
+
+        String[] command = new String[]{pythonExecutable, pythonScriptPath};
+
+        try {
+            ProcessBuilder pb = new ProcessBuilder(command);
+            Process process = pb.start();
+
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+
+            String s = null;
+            List<String> allLines = new ArrayList<>();
+
+            while ((s = stdInput.readLine()) != null) {
+                allLines.add(s);
+
+            }
+
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+
+            return allLines;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
---
+++ FillTheVoid/src/main/java/com/taip/FillTheVoid/restoration/restoration.py
@@ -0,0 +1,26 @@
+import base64
+import numpy as np
+import cv2
+
+if __name__ == '__main__':
+
+    file_path = "src/main/java/com/taip/FillTheVoid/restoration/restored_image.png"
+
+    image = cv2.imread(file_path)
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    height, width, channels = image.shape
+    print(f"Image Dimensions: {height}x{width}, Channels: {channels}")
+
+    # Display the image using OpenCV
+#     cv2.imshow("Restored Image", image)
+
+    result = cv2.imwrite(file_path, image_gray)
+
+
+    # Wait until a key is pressed and then close the window
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+    print("a")
+
